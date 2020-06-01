1d1cf8ddb5fc632440f10792ba247ceda87380d9
Pavel Ray <ashd@lowes.com>
2020-06-01 19:24:14 +0530

    added user sign in and sign out functionalities

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {connect} from 'react-redux';
+
 import {auth} from '../../firebase/firebase.utils';
 
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+export const Header = ({currentUser}) => (
     <div className='header'>
         <Link to='/' className='logo-container' >
             <Logo className='logo' />
@@ -22,6 +24,10 @@ const Header = ({currentUser}) => (
 
         </div>
     </div>
-)
+);
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import FormInput from '../../components/form-input/form-input.component';
 import CustomButton from '../../components/custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import {auth,  signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -16,10 +16,19 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = (event) =>{
+    handleSubmit = async (event) =>{
         event.preventDefault();
+        
+        const {email,password} = this.state;
 
-        this.setState({email: '', password: ''});
+        try{
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({ email: '', password: '' });
+
+        }catch(error){
+            console.log(error);
+        }
+
     }
 
     handleChange = event =>{
@@ -53,7 +62,7 @@ class SignIn extends React.Component {
                 />
                 <div className='buttons'>
                     <CustomButton type='submit'> Sign In </CustomButton>
-                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn> 
+                    <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn> 
                         Sign in with Google
                     </CustomButton>
                 </div>
